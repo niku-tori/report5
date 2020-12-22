29cde4faed21a3bbbcbc6ead33a29e23675b84ad
MUTO KAKU <e205706@ie.u-ryukyu.ac.jp>
2020-12-22 15:27:16 +0900

    2nd commit

--- src/jp/ac/uryukyu/ie/e205706/Main.java
+++ src/jp/ac/uryukyu/ie/e205706/Main.java
@@ -2,8 +2,13 @@ package jp.ac.uryukyu.ie.e205706;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
-    
 }
